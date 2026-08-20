db6a60a4c5feecbfd490268cb10068280e9919eb
ForgeC2 User <user@example.com>
2026-08-20 22:56:18 +0800

    chore(cleanup): batch D - CI align (add tokens/bundle/openapi/permissions checks, fix go test paths) + sync webdist after i18n prune

--- internal/webdist/dist/assets/page-B_BAz3g_.js
+++
@@ -1,1 +0,0 @@
-import{W as t}from"./index-BnOkhtdR.js";import"./vendor-react-CasLP3f6.js";import"./vendor-misc-BJX4Yuat.js";import"./vendor-router-bjcp5fWE.js";import"./vendor-sonner-CY3mybMB.js";import"./vendor-baseui-Czj7WBuQ.js";import"./vendor-icons-DNIJFJg_.js";import"./vendor-zod-BAGC-vYU.js";function c(){t("/toolkit")}export{c as default};
---
+++ internal/webdist/dist/assets/page-DI0K8Tnv.js
@@ -0,0 +1,1 @@
+import{W as o}from"./index-OidqONNN.js";import"./vendor-react-CasLP3f6.js";import"./vendor-misc-BJX4Yuat.js";import"./vendor-router-bjcp5fWE.js";import"./vendor-sonner-CY3mybMB.js";import"./vendor-baseui-Czj7WBuQ.js";import"./vendor-icons-DNIJFJg_.js";import"./vendor-zod-BAGC-vYU.js";function s(){o("/automation#tab=workflows")}export{s as default};
